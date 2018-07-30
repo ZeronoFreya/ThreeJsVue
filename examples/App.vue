2560e0b1ffe4b4d2d6958c3fc149f794f5567859
zeronofreya <zeronofreya@gmail.com>
2018-07-30 19:43:58 +0800

    Initial commit

---
+++ examples/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <view-layer 
+            @on-load="onLoad"></view-layer>
+        <div class="loading" v-show="loading"></div>
+    </div>
+</template>
+<script>
+import ViewLayer from './components/view-layer';
+export default {
+    data() {
+        return {
+            loading: true
+        }
+    },
+    methods: {
+        onLoad() {
+            this.loading = false;
+        }
+    },
+    components: {
+        ViewLayer
+    }
+}
+</script>
+<style>
+@import "./static/style/base.scss";
+</style>
---
+++ examples/components/view-layer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <model-obj :backgroundAlpha="0"
+            @on-load="onLoad" 
+            src="../static/models/obj/tree.obj"></model-obj>
+    </div>
+</template>
+<script>
+import ModelObj from "../../src/model-obj.vue";
+
+export default {
+  name: "demo-obj",
+  data() {
+    return {};
+  },
+  methods: {
+    onLoad() {
+        this.$emit('on-load')
+    }
+  },
+  components: {
+    ModelObj
+  }
+};
+</script>
